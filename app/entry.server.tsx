925cf3fa6c1c7cd9d554ae2897c31e397659f106
senol keskin <senolk@lidyateknoloji.com>
2022-09-14 23:26:45 +0300

    clean some codes

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,12 +1,10 @@
-import { renderToString } from "react-dom/server"
-import { CacheProvider } from "@emotion/react"
-import createEmotionServer from "@emotion/server/create-instance"
+import { PassThrough } from "stream"
 import type { EntryContext } from "@remix-run/node"
+import { Response } from "@remix-run/node"
 import { RemixServer } from "@remix-run/react"
-import "dotenv/config"
+import { renderToPipeableStream } from "react-dom/server"
 
-import { ServerStyleContext } from "~/lib/emotion/context"
-import { createEmotionCache } from "~/lib/emotion/createEmotionCache"
+const ABORT_DELAY = 5000
 
 export default function handleRequest(
   request: Request,
@@ -14,31 +12,34 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const cache = createEmotionCache()
-  const { extractCriticalToChunks } = createEmotionServer(cache)
+  return new Promise((resolve, reject) => {
+    let didError = false
 
-  const html = renderToString(
-    <ServerStyleContext.Provider value={null}>
-      <CacheProvider value={cache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </CacheProvider>
-    </ServerStyleContext.Provider>,
-  )
+    const { pipe, abort } = renderToPipeableStream(<RemixServer context={remixContext} url={request.url} />, {
+      onShellReady: () => {
+        const body = new PassThrough()
 
-  const chunks = extractCriticalToChunks(html)
+        responseHeaders.set("Content-Type", "text/html")
 
-  const markup = renderToString(
-    <ServerStyleContext.Provider value={chunks.styles}>
-      <CacheProvider value={cache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </CacheProvider>
-    </ServerStyleContext.Provider>,
-  )
+        resolve(
+          new Response(body, {
+            headers: responseHeaders,
+            status: didError ? 500 : responseStatusCode,
+          }),
+        )
 
-  responseHeaders.set("Content-Type", "text/html")
+        pipe(body)
+      },
+      onShellError: (err) => {
+        reject(err)
+      },
+      onError: (error) => {
+        didError = true
 
-  return new Response(`<!DOCTYPE html>${markup}`, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+        console.error(error)
+      },
+    })
+
+    setTimeout(abort, ABORT_DELAY)
   })
 }
--- app/lib/remix.ts
+++
@@ -1,4 +0,0 @@
-import { json } from "@remix-run/node"
-
-export const badRequest = (data: any) => json(data, { status: 400 })
-export const notFound = (data: any) => json(data, { status: 404 })
