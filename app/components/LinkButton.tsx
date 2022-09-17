8c949c826cad9914c71db38a5ba1066163c61c41
senol keskin <senolk@lidyateknoloji.com>
2022-09-17 14:23:52 +0300

    feat: prop types

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@chakra-ui/react"
+import { Button, ButtonProps } from "@chakra-ui/react"
 import { Link } from "@remix-run/react"
 
-export const LinkButton = (props) => {
+interface Props extends ButtonProps {
+  to: string
+}
+
+export const LinkButton: React.FC<Props> = (props) => {
   return (
     <Button as={Link} {...props}>
       {props.children}
